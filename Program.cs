4cef8cb42ee3f9352278b82e95458a462c81defb
DatLT <datlthe151200@fpt.edu.vn>
2024-03-20 08:35:44 +0700

    Update configurations

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using project231.Authenticate;
 using project231_2.Models;
 using project231_2.Repository;
@@ -31,7 +32,34 @@ namespace project231_2
             });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
             builder.Services.AddScoped<IGetInventoryRepository, GetInventoryRepo>();
             builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
@@ -40,7 +68,7 @@ namespace project231_2
             ConfigurationManager configuration = builder.Configuration;
             builder.Services.AddDbContext<Project231Context>(options => options.UseSqlServer(configuration.GetConnectionString("PRN231_Project")));
 
-        
+
 
             builder.Services.AddAuthentication(options =>
             {
